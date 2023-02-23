cbce125fcf2590c966d6d6266684de46995e0215
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-02-23 21:53:10 +0530

    removed bug in creation of custom thread

--- interdomain_KPIs_Data_Prep_Task.py
+++ interdomain_KPIs_Data_Prep_Task.py
@@ -19,7 +19,7 @@ class CustomThread(Thread):
 
     def run(self):
         if(self._target is not None):
-            self._return = self.target(*self._args, **self._kwargs)
+            self._return = self._target(*self._args, **self._kwargs)
     
     def join(self):
         Thread.join(self)
