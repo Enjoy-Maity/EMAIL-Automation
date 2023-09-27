4269d2e93d76f035d9904f2302cea5158e5aa10a
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2023-09-27 21:47:57 +0530

    created module for handling CustomExceptions

---
+++ Custom_Exception.py
@@ -0,0 +1,8 @@
+from tkinter import messagebox
+
+class CustomException(Exception):
+    def __init__(self,title,message):
+        self.title = title
+        self.message = message
+        super().__init__(self.title,self.message)
+        messagebox.showerror(self.title,self.message)
